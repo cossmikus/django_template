799b241677a641031bd00fc8b6c5f81c13e7063f
cossmikus <kassymkhan.bolat@nu.edu.kz>
2024-04-04 01:09:04 +0500

    Fix incorrect directory issue aneeees

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,11 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
-from .views import hello_world  # Import the view
+from .views import hello_world
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('hello/', hello_world),  # Map the view to a URL
+    path('hello', hello_world, name='hello-world'),
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+@api_view(['GET'])
 def hello_world(request):
-    return HttpResponse("Hello, world!")
+    return Response("Hello World")
