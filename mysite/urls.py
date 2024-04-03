2670eef3b5e7eb6ec3df64088b29556c3eaf0bc7
cossmikus <kassymkhan.bolat@nu.edu.kz>
2024-04-04 01:34:09 +0500

    upd3

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,9 +17,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getUsers),
-    path('create', views.addUser),
-    path('read/<str:pk>', views.getUser),
-    path('update/<str:pk>', views.updateUser),
-    path('delete/<str:pk>', views.deleteUser),
+    path('users/', views.getUsers),
+    path('users/create', views.addUser),
+    path('users/read/<str:pk>', views.getUser),
+    path('users/update/<str:pk>', views.updateUser),
+    path('users/delete/<str:pk>', views.deleteUser),
 ]
+
